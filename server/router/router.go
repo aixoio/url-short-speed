b11ef9c9d5f549f8697f8fafcd83ef0e40ca4669
David L Terpin <79969530+aixoio@users.noreply.github.com>
2024-06-30 22:31:37 +1000

    Updated router configuration in server/router/router.go

--- server/router/router.go
+++ server/router/router.go
@@ -6,10 +6,13 @@ import (
 
 	"github.com/aixoio/url-short-speed/server/router/handlers"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 func StartRouter() {
 	app := fiber.New()
+
+	app.Use(logger.New())
 
 	app.Post("/api/link/new", handlers.NewLinkHandler)
 	app.Get("/api/follow/:short", handlers.FollowLinkHandler)
