c3c1606f1acdffc60ec2aa31244275f8bdbefe72
David L Terpin <79969530+aixoio@users.noreply.github.com>
2024-06-30 22:29:45 +1000

    Update server/main.go

--- server/main.go
+++ server/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"github.com/aixoio/url-short-speed/server/db"
+	"github.com/aixoio/url-short-speed/server/router"
 	"github.com/joho/godotenv"
 )
 
 func main() {
 	godotenv.Load()
 	db.Connect()
+	router.StartRouter()
 }
