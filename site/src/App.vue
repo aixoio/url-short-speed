d2ae8bffedc657022d8fa6b4ad3e48eef2cd850f
David L Terpin <79969530+aixoio@users.noreply.github.com>
2024-06-30 22:39:06 +1000

    Update App.vue file

--- site/src/App.vue
+++ site/src/App.vue
@@ -3,5 +3,11 @@
     <div class="flex justify-center">
       <h1 class="text-3xl">URL short</h1>
     </div>
+    <div class="flex justify-center">
+      <div class="grid grid-cols-1">
+        <input type="text" placeholder="URL to shorten here!" class="outline-none border border-gray-950 p-2 m-3 rounded-lg">
+        <button class="bg-blue-500 p-2 rounded-lg text-white">Shorten the link!</button>
+      </div>
+    </div>
   </div>
 </template>
