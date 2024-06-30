664fc06c4db60de97431f88053ea98319ab78d73
David L Terpin <79969530+aixoio@users.noreply.github.com>
2024-06-30 22:39:42 +1000

    Update App.vue file

--- site/src/App.vue
+++ site/src/App.vue
@@ -5,9 +5,18 @@
     </div>
     <div class="flex justify-center">
       <div class="grid grid-cols-1">
-        <input type="text" placeholder="URL to shorten here!" class="outline-none border border-gray-950 p-2 m-3 rounded-lg">
+        <input type="text" placeholder="URL to shorten here!"
+          class="outline-none border border-gray-950 p-2 m-3 rounded-lg" v-model="longurl">
         <button class="bg-blue-500 p-2 rounded-lg text-white">Shorten the link!</button>
       </div>
     </div>
   </div>
 </template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+const longurl = ref("");
+
+
+</script>
